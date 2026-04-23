534256a4f5739f53421c99b83c2c151b05e97d4b
Tohzt <steven.g.french@live.com>
2026-04-23 16:50:18 -0700

    grip gun broken

--- Objects/gun_controller.gd
+++ Objects/gun_controller.gd
@@ -1,11 +1,23 @@
 extends Node3D
-@onready var gun: XRToolsPickable = $gun
+
+@onready var gun := $gun
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@export var bullet_scene: PackedScene = preload("res://Objects/bullet.tscn")
+@export var bullet_speed: float = 5.0
+
+@onready var muzzle: Marker3D = $Marker3D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	gun.action_pressed.connect(_on_action_pressed)
 
+func _on_action_pressed(_pickable) -> void:
+	var spawn_transform := muzzle.global_transform
 
-func _process(delta: float) -> void:
-	if gun.action_pressed:
-		animation_player.play("Backfire")
+	animation_player.play("Recoil")
+
+	var bullet = bullet_scene.instantiate()
+	get_tree().current_scene.add_child(bullet)
+
+	bullet.global_transform = spawn_transform
+	bullet.apply_central_impulse(-spawn_transform.basis.z * bullet_speed)
