f558426fc9c80849ca2e83bd1abe2f0db60a2ddf
Tohzt <steven.g.french@live.com>
2026-04-23 17:32:57 -0700

    gun go pewpew

--- Objects/gun_controller.gd
+++ Objects/gun_controller.gd
@@ -5,19 +5,17 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 @export var bullet_scene: PackedScene = preload("res://Objects/bullet.tscn")
-@export var bullet_speed: float = 5.0
+@export var bullet_speed: float = 12.0
 
 
 func _ready() -> void:
 	gun.action_pressed.connect(_on_action_pressed)
 
 func _on_action_pressed(_pickable) -> void:
-	var spawn_transform := bullet_origin.global_transform
-
-	animation_player.play("Recoil")
+	#animation_player.play("Recoil")
 
 	var bullet = bullet_scene.instantiate()
 	get_tree().current_scene.add_child(bullet)
 
-	bullet.global_transform = spawn_transform
-	bullet.apply_central_impulse(-spawn_transform.basis.z * bullet_speed)
+	bullet.global_transform = bullet_origin.global_transform
+	bullet.apply_central_impulse(bullet_origin.global_transform.basis.z * bullet_speed)
